27d69a82ec9f8286cfb3b18dcdbb58eef13b10e6
DeAnna Martinez <deannapi.mart@gmail.com>
2020-04-13 23:15:25 -0500

    refactored - removed next button. auto load to next.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,93 +1,86 @@
 const startButton = document.getElementById('start');
+var right = document.getElementById("response");
+var redo = document.getElementById("replay");
 
 // questions
 var questions = [
-  {
-      "question": "Which is a JavaScript Data Type?",
-      "option1": "number",
-      "option2": "string",
-      "option3": "boolean",
-      "option4": "all of the above",
-      "answer": "4"
-  },
-  {
-      "question": "What company developed JavaScript?",
-      "option1": "Java Inc.",
-      "option2": "Netscape",
-      "option3": "JQuery",
-      "option4": "CERN",
-      "answer": "2"
-  },
-  {
-      "question": "Which is correct to comment out lines in JS?",
-      "option1": "/*",
-      "option2": "#",
-      "option3": "//",
-      "option4": "<!---->",
-      "answer": "3"
-  },
-  {
-      "question": "Which of the following returns 'pink' for var colors = ['red', 'orange', 'pink']?",
-      "option1": "colors[2]",
-      "option2": "colors[0]",
-      "option3": "colors[1]",
-      "option4": "colors[3]",
-      "answer": "1"
-  },
-  {
-      "question": "What statement exits from a loop?",
-      "option1": "pass;",
-      "option2": "break;",
-      "option3": "return;",
-      "option4": "exit;",
-      "answer": "2"
-  },
-  {
-      "question": "Inside which HTML element does the JavaScript link belong?",
-      "option1": "<link>",
-      "option2": "<meta>",
-      "option3": "<script>",
-      "option4": "<footer>",
-      "answer": "3"
-  },
-  {
-      "question": "Which is the correct method to hyperlink?",
-      "option1": "<h2>Hyperlink</h2>",
-      "option2": "<div href=\"url\">Hyperlink</div>",
-      "option3": "<link>Hyperlink</link>",
-      "option4": "<a href=\"url\">Hyperlink</a>",
-      "answer": "4"
-  },
+  new Question(
+      "1. Which is a JavaScript Data Type?",
+      ["number",
+      "string",
+      "boolean",
+      "all of the above"],
+      "4"
+  ),
+  new Question(
+      "2. What company developed JavaScript?",
+      ["Java Inc.",
+      "Netscape",
+      "JQuery",
+      "CERN"],
+      "2"
+  ),
+  new Question(
+      "3. Which is correct to comment out lines in JS?",
+      ["/*",
+      "#",
+      "//",
+      "<!-- -->"],
+      "3"
+  ),
+  new Question(
+      "4. Which of the following returns 'pink' for var colors = ['red', 'orange', 'pink']?",
+      ["colors[2]",
+      "colors[0]",
+      "colors[1]",
+      "colors[3]"],
+      "1"
+  ),
+  new Question(
+      "5. What statement exits from a loop?",
+      ["pass;",
+      "break;",
+      "return;",
+      "exit;"],
+      "2"
+  ),
+  new Question(
+      "6. Inside which HTML element does the JavaScript link belong?",
+      ["<link>",
+      "<meta>",
+      "<script>",
+      "<footer>"],
+      "3"
+  ),
+  new Question(
+      "7. Which is the correct method to hyperlink?",
+      ["<h2>Hyperlink</h2>",
+      "<div href=\"url\">Hyperlink</div>",
+      "<link>Hyperlink</link>",
+      "<a href=\"url\">Hyperlink</a>"],
+      "4"
+  ),
 ]
 
 //timer countdown
-var timerRef = null;
-
-// startTimer that gets called upon start button being clicked
-// startTimer function only contains the setTimeout function
-// within the setTimeout function we create a new function that does the work that is currently inside our startTimer()
-
 function startTimer() {
   var p_time = document.getElementById('time').innerHTML;
-  var timeArray = p_time.split(/[:]+/);
+  var timeArray = p_time.split(":");
   var min = timeArray[0];
   var sec = checkSecond((timeArray[1] - 1));
-  if (sec === "59") {
+  if (sec == 59) {
     min = min - 1
   }
 
-  document.getElementById('time').innerHTML = min + ":" + sec;
-
-  if (min <= 0 && sec == 0) {
+  if (min < 0) {
     clearTimeout(timerRef)
     alert("Time is up!")
     return;
   }
 
-  if(min === 1 && sec === 0) {
-    min = 0;
-  }
-  setTimeout(startTimer, 1000);
+  document.getElementById('time').innerHTML = min + ":" + sec;
+
+  var timerRef = setTimeout(startTimer, 1000);
 }
 
 function checkSecond(seconds) {
@@ -102,7 +95,7 @@ function checkSecond(seconds) {
 
 function wrongTimer () {
   var x_time = document.getElementById('time').innerHTML;
-  var wrongArray = x_time.split(/[:]+/);
+  var wrongArray = x_time.split(":");
   var x_min = wrongArray[0];
   var x_sec = checkSecond((wrongArray[1]-1));
   if(x_sec === 59) {
@@ -112,37 +105,81 @@ function wrongTimer () {
   document.getElementById('time').innerHTML = x_min + ":" + x_sec;
 }
 
-var currentQuestion =0;
-var score = 0;
-var container = document.getElementById("quizContainer");
-var quesEl = document.getElementById("question");
-var opt1 = document.getElementById("opt1");
-var opt2 = document.getElementById("opt2");
-var opt3 = document.getElementById("opt3");
-var opt4 = document.getElementById("opt4");
-var totalQues = questions.length;
-var nextButton = document.getElementById("nextButton");
-var resultCont = document.getElementById("result");
-var redo = document.getElementById("replay");
+function stopTimer () {
+  clearInterval(timerRef);
+}
 
-function loadQues (questionIndex) {
-  var q = questions[questionIndex];
-  quesEl.textContent = (questionIndex +1) + '. ' + q.question;
-  opt1.textContent = q.option1;
-  opt2.textContent = q.option2;
-  opt3.textContent = q.option3;
-  opt4.textContent = q.option4;
+function Quiz(questions) {
+  this.score = 0;
+  this.questions = questions;
+  this.questionIndex =0;
+}
+
+Quiz.prototype.getQuestionIndex = function () {
+  return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function (answer) {
+  if(this.getQuestionIndex().isCorrectAnswer(answer)) {   
+    this.score += 10;
+  } 
+  this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function() {
+  return this.questionIndex === this.questions.length;
+}
+
+function Question(text, choices, answer) {
+  this.text = text;
+  this.choices = choices;
+  this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function(choice) {
+  return this.answer === choice;
+}
+
+function populate() {
+  if(quiz.isEnded()) {
+    showScores();
+  }
+  else {
+    var quesEl = document.getElementById("question");
+    quesEl.innerHTML = quiz.getQuestionIndex().text;
+
+    var choices = quiz.getQuestionIndex().choices;
+    for(var i =0; i < choices.length; i++) {
+      var quesEl = document.getElementById("choice"+i);
+      quesEl.innerHTML = choices[i];
+      guess("btn" + i, choices[i]);
+    }
+  }
 };
 
-function LoadNextQuestion () {
-  var selectedOpt = document.querySelector("input[type=radio]:checked");
-  var right = document.getElementById('response');
-  if(!selectedOpt) {
-    alert("Select an answer.");
-    return;
+function guess(id, guess) {
+  var button = document.getElementById(id);
+  button.onclick = function () {
+      quiz.guess(guess);
+      populate();
   }
-  var answer = selectedOpt.value;
-  if(questions[currentQuestion].answer === answer) {
+};
+
+function showScores() {
+  var initials = window.prompt("Enter your initials.")
+  var gameOverHTML = "<h1>Result</h1>";
+  gameOverHTML += "<h2 id='score'> Your scores: " + initials + " " + quiz.score + "</h2>";
+  localStorage.setItem("highscores", initials + " " + quiz.score);
+  var element = document.getElementById("quiz");
+  element.innerHTML = gameOverHTML;
+};
+
+//create quiz
+var quiz = new Quiz(questions);
+
+function answerSel () {
+  var userAnswer =  ;
+  if(questions[currentQuestion].answer === userAnswer) {
     // alert correct
     score += 10;
     right.innerHTML="Correct!";
@@ -151,24 +188,6 @@ function LoadNextQuestion () {
     wrongTimer();
     right.innerHTML="Incorrect! -10 seconds!";
   }
-  
-  selectedOpt.checked = false;
-  currentQuestion++;
-  if(currentQuestion== totalQues-1) {
-    nextButton.textContent="Submit";
-  }
-  if(currentQuestion == totalQues) {
-    container.style.display = "none";
-    resultCont.style.display='';
-    startButton.style.display = 'none';
-    var initials = window.prompt("Enter your initials.");
-    localStorage.setItem("highscore", initials + " " + score);
-    resultCont.textContent="Score: " + initials + " " + score;
-    redo.textContent="Replay";
-    clearTimeout(timerRef);    
-    return;
-  }
-  loadQues(currentQuestion);
 };
 
 function replay () {
@@ -176,7 +195,5 @@ function replay () {
 };
 
 startButton.addEventListener('click', startTimer);
-startButton.addEventListener('click',loadQues(currentQuestion));
+startButton.addEventListener('click',populate);
 redo.addEventListener('click', replay);
-
-// fix media pages
